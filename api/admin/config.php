a56a189c6f491c718d477786ade6e56968899a4d
jazzman54f <159256884+jazzman54f@users.noreply.github.com>
2026-06-08 22:10:37 +0100

    Delete api/admin directory

--- api/admin/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Change these credentials and regenerate the hash when deploying.
-const ADMIN_USERNAME = 'admin';
-const ADMIN_PASSWORD_HASH = '$2y$12$PXty9kZWiaBW5ZbtDclB6Ob.K3dJIbVkwHdV8tEsYkjJhuj4MvEN6';
